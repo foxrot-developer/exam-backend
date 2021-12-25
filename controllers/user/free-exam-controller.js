7021f725d0557e534fdf5870abf86db703fb6b7c
Usama <usama.bashirb1@gmail.com>
2021-12-26 00:52:00 +0500

    Changes

--- controllers/user/free-exam-controller.js
+++ controllers/user/free-exam-controller.js
@@ -119,7 +119,7 @@ const freeExamScore = async (req, res, next) => {
                 return next(new HttpError('Question id is required', 422));
             }
             const finalAnswers = await ArFreeExam.findOne({ enId: answer.id });
-            if (!finalAnswers.draggable) {
+            if (!answer.draggable) {
                 if (answer.answer === finalAnswers.answer) {
                     return {
                         id: answer.id,
@@ -170,7 +170,7 @@ const freeExamScore = async (req, res, next) => {
                 return next(new HttpError('Question id is required', 422));
             }
             const finalAnswers = await NlFreeExam.findOne({ enId: answer.id });
-            if (!finalAnswers.draggable) {
+            if (!answer.draggable) {
                 if (answer.answer === finalAnswers.answer) {
                     return {
                         id: answer.id,
