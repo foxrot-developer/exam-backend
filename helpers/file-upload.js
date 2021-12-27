61fe555844c8e44ae3dea6fe1306498fb1a03557
Usama <usama.bashirb1@gmail.com>
2021-12-27 20:50:44 +0500

    Changes

--- helpers/file-upload.js
+++ helpers/file-upload.js
@@ -1,7 +1,5 @@
 const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
-const path = require('path');
-
 
 const MIME_TYPE_MAP = {
     'image/png': 'png',
@@ -10,10 +8,10 @@ const MIME_TYPE_MAP = {
 };
 
 const fileUpload = multer({
-    limits: 50000000,
+    limits: 500000,
     storage: multer.diskStorage({
         destination: (req, file, cb) => {
-            cb(null, path.join(__dirname, 'uploads/images'));
+            cb(null, 'uploads/images');
         },
         filename: (req, file, cb) => {
             const ext = MIME_TYPE_MAP[file.mimetype];
