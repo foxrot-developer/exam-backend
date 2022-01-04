dd683fb24328532108c29b1d4fa6d84ce0ffc10c
Usama <usama.bashirb1@gmail.com>
2022-01-05 00:54:23 +0500

    Changes

--- controllers/user/free-exam-controller.js
+++ controllers/user/free-exam-controller.js
@@ -211,12 +211,18 @@ const freeExamScore = async (req, res, next) => {
         }
 
         let allQuestions = [];
-        ques_part1 = JSON.parse(existingArFreeExam[0].part1);
-        allQuestions.push(ques_part1);
-        ques_part2 = JSON.parse(existingArFreeExam[0].part2);
-        allQuestions.push(ques_part2);
-        ques_part3 = JSON.parse(existingArFreeExam[0].part3);
-        allQuestions.push(ques_part3);
+        ques_part1 = JSON.parse(existingFreeExam[0].part1);
+        ques_part1.forEach(element => {
+            allQuestions.push(element);
+        });
+        ques_part2 = JSON.parse(existingFreeExam[0].part2);
+        ques_part2.forEach(element => {
+            allQuestions.push(element);
+        });
+        ques_part3 = JSON.parse(existingFreeExam[0].part3);
+        ques_part3.forEach(element => {
+            allQuestions.push(element);
+        });
 
         const questionAnswers = answers.map(async answer => {
             if (!answer.id) {
@@ -294,12 +300,18 @@ const freeExamScore = async (req, res, next) => {
         }
 
         let allQuestions = [];
-        ques_part1 = JSON.parse(existingNlFreeExam[0].part1);
-        allQuestions.push(ques_part1);
-        ques_part2 = JSON.parse(existingNlFreeExam[0].part2);
-        allQuestions.push(ques_part2);
-        ques_part3 = JSON.parse(existingNlFreeExam[0].part3);
-        allQuestions.push(ques_part3);
+        ques_part1 = JSON.parse(existingFreeExam[0].part1);
+        ques_part1.forEach(element => {
+            allQuestions.push(element);
+        });
+        ques_part2 = JSON.parse(existingFreeExam[0].part2);
+        ques_part2.forEach(element => {
+            allQuestions.push(element);
+        });
+        ques_part3 = JSON.parse(existingFreeExam[0].part3);
+        ques_part3.forEach(element => {
+            allQuestions.push(element);
+        });
 
 
         const questionAnswers = answers.map(async answer => {
